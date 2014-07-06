165940f9702c756bf42e825c81852569bfa6e4a4
lfybkf <lfybkfa@gmail.com>
2014-07-06 14:47:14 +0300

    execs now list

--- Kommand/Kommand.cs
+++ Kommand/Kommand.cs
@@ -9,11 +9,16 @@ namespace BDB
 {
 	public class Kommand
 	{
+		//event
+		public event EventHandler isExecuted;
+		protected void fireIsExecuted() { if (isExecuted != null) { isExecuted.Invoke(this, null); } }
+
 		//public
 		internal readonly string Name = String.Empty;
-		internal readonly Action Exec = null;
-		internal readonly Keys Scut = Keys.None;
+		internal readonly Keys keys = Keys.None;
+		internal IList<Action> Execs = new List<Action>();
 		public string Caption = String.Empty;
+		public Shortcut shortCut { get { return getShortcut(keys); } }
 
 		//static
 		public static Kommand Empty = new Kommand();
@@ -26,32 +31,43 @@ namespace BDB
 		{
 			this.Name = Name;
 			this.Caption = Name;
-			this.Exec = Exec;
-			this.Scut = Keys.None;
+			this.keys = Keys.None;
+			if (Exec != null)	Execs.Add(Exec);
 		}//func
 
-		public Kommand(string Name, Action Exec, Keys Scut)
+		public Kommand(string Name, Action Exec, Keys keys)
 		{
 			this.Name = Name;
 			this.Caption = Name;
-			this.Exec = Exec;
-			this.Scut = Scut;
+			this.keys = keys;
+			if (Exec != null) Execs.Add(Exec);
 		}//func
 
-		public Kommand(string Name, string Caption, Action Exec, Keys Scut)
+		public Kommand(string Name, string Caption, Action Exec, Keys keys)
 		{
 			this.Name = Name;
 			this.Caption = Caption;
-			this.Exec = Exec;
-			this.Scut = Scut;
+			this.keys = keys;
+			if (Exec != null) Execs.Add(Exec);
 		}//func
+
+		public Kommand(string Name, Keys keys)
+		{
+			this.Name = Name;
+			this.Caption = Name;
+			this.keys = keys;
+		}//func
+
+		public Kommand Add(Action Exec)
+		{
+			if (Exec != null) Execs.Add(Exec);
+			return this;
+		}//functoin
 
 		public void Execute()
 		{
-			if (Exec != null)
-			{
-				Exec();
-			}//if
+			foreach (var action in Execs)	{ action(); }
+			fireIsExecuted();
 		}//func
 
 		public static void Execute(Kommand kmd)		{if (kmd != null) { kmd.Execute(); }}//func
@@ -83,33 +99,29 @@ namespace BDB
 			return Ret;
 		}//func		
 
-
 		#region shortcut
-		public static Shortcut GetShortcut(Keys key, Keys mods)
+		public static Shortcut getShortcut(Keys key)
 		{
-			if (mods.HasFlag(Keys.Control))
+			if (key.HasFlag(Keys.Control))
 			{
-				if (key == Keys.C)
-					return Shortcut.CtrlC;
-				else if (key == Keys.O)
-					return Shortcut.CtrlO;
-				else if (key == Keys.S)
-					return Shortcut.CtrlS;
-				else if (key == Keys.V)
-					return Shortcut.CtrlV;
-				else if (key == Keys.Z)
-					return Shortcut.CtrlZ;
-				else if (key == Keys.Delete)
-					return Shortcut.CtrlDel;
-				else if (key == Keys.Insert)
-					return Shortcut.CtrlIns;
+				if (key == Keys.A)				return Shortcut.CtrlA;
+				else if (key == Keys.C)			return Shortcut.CtrlC;
+				else if (key == Keys.E)			return Shortcut.CtrlE;
+				else if (key == Keys.F)			return Shortcut.CtrlF;
+				else if (key == Keys.H)			return Shortcut.CtrlH;
+				else if (key == Keys.I)			return Shortcut.CtrlI;
+				else if (key == Keys.O)			return Shortcut.CtrlO;
+				else if (key == Keys.R)			return Shortcut.CtrlR;
+				else if (key == Keys.S)			return Shortcut.CtrlS;
+				else if (key == Keys.V)			return Shortcut.CtrlV;
+				else if (key == Keys.Z)			return Shortcut.CtrlZ;
+				else if (key == Keys.Delete)	return Shortcut.CtrlDel;
+				else if (key == Keys.Insert)	return Shortcut.CtrlIns;
 			}//if
-			else if (mods.HasFlag(Keys.Shift))
+			else if (key.HasFlag(Keys.Shift))
 			{
-				if (key == Keys.Insert)
-					return Shortcut.ShiftIns;
-				else if (key == Keys.Delete)
-					return Shortcut.ShiftDel;
+				if (key == Keys.Insert)			return Shortcut.ShiftIns;
+				else if (key == Keys.Delete)	return Shortcut.ShiftDel;
 			}//if
 
 			return Shortcut.None;
--- Kommand/ListExtension.cs
+++ Kommand/ListExtension.cs
@@ -14,7 +14,7 @@ namespace BDB
 		public static String PrefixButton = "btn";
 		public static String PrefixMenu = "mi";
 
-		public static Kommand get(this IEnumerable<Kommand> lst, Keys scut) { return lst.get(k => k.Scut == scut); }//func
+		public static Kommand get(this IEnumerable<Kommand> lst, Keys keys) { return lst.get(k => k.keys == keys); }//func
 		public static Kommand get(this IEnumerable<Kommand> lst, string Name) { return lst.get(k => k.Name == Name); }//func
 
 		public static Kommand get(this IEnumerable<Kommand> lst, Func<Kommand, bool> Match)
@@ -43,7 +43,7 @@ namespace BDB
 					ctl.Tag = kmd;
 					ctl.Click += click;
 					if (ctl is ToolStripMenuItem)
-						(ctl as ToolStripMenuItem).ShortcutKeys = kmd.Scut;
+						(ctl as ToolStripMenuItem).ShortcutKeys = kmd.keys;
 				}//if
 			}
 			else if (cmp is Button)
