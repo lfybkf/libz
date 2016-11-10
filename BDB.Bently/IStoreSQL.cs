d94fcf3543226ec59dd51be53d4d70d54cbbbc3c
Булычев Даниил Борисович <bulychev.d@aetp.nn>
2016-11-10 15:45:38 +0300

    (bdb) doc

--- BDB.Bently/IStoreSQL.cs
+++ BDB.Bently/IStoreSQL.cs
@@ -18,11 +18,13 @@ namespace BDB
 		DbCommand getCommand();
 		DbParameter getParameter();
 		DbDataReader OpenReader(DbCommand cmd);
+		///<summary>ошибка при последнем выполнении</summary> 
 		Exception LastError { get; set; }
 	}//interface
 
 	public static class Extension_IStoreSQL
 	{
+		///<summary>получить команду и заполнить CommandText = sql</summary> 
 		public static DbCommand getCommand(this IStoreSQL store, string sql)
 		{
 			var cmd = store.getCommand();
@@ -30,6 +32,7 @@ namespace BDB
 			return cmd;
 		}
 
+		///<summary>Получить заполненный параметр нужного типа. Если он может содержать NULL и Value==NULL, то значение параметра = DBNull.Value</summary> 
 		public static DbParameter getParameter(this IStoreSQL store, string Name, object Value, bool IsNullable = false)
 		{
 			var result = store.getParameter();
@@ -45,19 +48,21 @@ namespace BDB
 			
 			return result;
 		}
-
+		///<summary>выполнить команду sql</summary> 
 		public static bool Execute(this IStoreSQL store, string sql)
 		{
 			var cmd = store.getCommand(sql);
 			return store.Execute(cmd);
 		}
-		
+
+		///<summary>получить скаляр</summary>
 		public static object Scalar(this IStoreSQL store, string sql)
 		{
 			var cmd = store.getCommand(sql);
 			return store.Scalar(cmd);
 		}
-		
+
+		///<summary>получить скаляр</summary>
 		public static object Scalar(this IStoreSQL store, DbCommand cmd)
 		{
 			object result = null;
@@ -78,21 +83,25 @@ namespace BDB
 			return result;
 		}//function
 
+		///<summary>получить скаляр и привести к типу Т</summary>
 		public static T Scalar<T>(this IStoreSQL store, DbCommand cmd, T def = default(T))
 		{
 			object result = store.Scalar(cmd);
 			return result != null ? (T)Convert.ChangeType(result, typeof(T)) : def;
 		}//function
 
+		///<summary>получить скаляр и привести к типу Т</summary>
 		public static T Scalar<T>(this IStoreSQL store, string sql, T def = default(T))
 		{
 			var cmd = store.getCommand(sql);
 			return store.Scalar<T>(cmd);
 		}//function
 
+		///<summary>получить массив скаляров и привести их к типу Т</summary>
 		public static IEnumerable<T> Scalars<T>(this IStoreSQL store, string sql)
 		{ return store.Scalars<T>(store.getCommand(sql)); }
 
+		///<summary>получить массив скаляров и привести их к типу Т</summary>
 		public static IEnumerable<T> Scalars<T>(this IStoreSQL store, DbCommand cmd)
 		{
 			List<T> result = new List<T>();
@@ -123,12 +132,13 @@ namespace BDB
 			return result;
 		}//function
 
+		///<summary>получить максимум</summary> 
 		public static T Max<T>(this IStoreSQL store, string Table, string Field, T def = default(T))
 		{
 			DbCommand cmd = store.getCommand(string.Format("SELECT MAX({0}) FROM {1}", Field, Table));
 			return store.Scalar<T>(cmd, def);
 		}//function
-		
+		///<summary>получить минимум</summary> 
 		public static T Min<T>(this IStoreSQL store, string Table, string Field, T def = default(T))
 		{
 			DbCommand cmd = store.getCommand(string.Format("SELECT MIN({0}) FROM {1}", Field, Table));
