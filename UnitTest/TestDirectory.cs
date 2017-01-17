2d9887b365823fe2d57df51a637d3e1ff5496ce5
lfybkf <lfybkf@mail.ru>
2017-01-18 00:39:41 +0300

    now

---
+++ UnitTest/TestDirectory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BDB;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class TestDirectory
+	{
+		[TestMethod]
+		public void testDir()
+		{
+			string path = @"C:\temp";
+			var di = new System.IO.DirectoryInfo(path);
+			string dir1 = di.add("first", "second");
+			string sub = di.subPath(dir1);
+			return;
+		}
+	}//class
+}//namespace
