4fc7b111fb007b24432fa61a4a51582fdbe74f14
lfybkf <lfybkf@mail.ru>
2017-01-19 01:07:54 +0300

    now

--- BDB.Bently/SqlF_manual.cs
+++ BDB.Bently/SqlF_manual.cs
@@ -8,25 +8,23 @@ namespace BDB
 {
 	public static partial class SqlF
 	{
-		private const string NOT = "NOT";
+		private static string not(bool IsPositive) => IsPositive ? string.Empty : "NOT";
+
 		///<summary>COUNT(*)</summary> 
 		public const string COUNT = "COUNT(*)";
 
 		///<summary>соединить условия из списка по И</summary>
-		public static string AND(IEnumerable<string> whereS) { return string.Join(") and (", whereS).quote(C.LParenthesis); }//function
+		public static string AND(IEnumerable<string> whereS) => string.Join(") and (", whereS).quote(C.LParenthesis);
 		///<summary>соединить условия из списка по И</summary>
-		public static string AND(params string[] whereS) { return string.Join(") and (", whereS).quote(C.LParenthesis); }//function
+		public static string AND(params string[] whereS) => string.Join(") and (", whereS).quote(C.LParenthesis);
 
 		///<summary>соединить условия из списка по ИЛИ</summary>
-		public static string OR(IEnumerable<string> whereS) { return string.Join(") or (", whereS).quote(C.LParenthesis); }//function
+		public static string OR(IEnumerable<string> whereS) => string.Join(") or (", whereS).quote(C.LParenthesis);
 		///<summary>соединить условия из списка по ИЛИ</summary>
-		public static string OR(params string[] whereS) { return string.Join(") or (", whereS).quote(C.LParenthesis); }//function
+		public static string OR(params string[] whereS) => string.Join(") or (", whereS).quote(C.LParenthesis);
 
 		///<summary>fld IS NULL</summary>
-		public static string IsNull(string fld, bool IsPositive = true) {
-			string sNot = IsPositive ? string.Empty : NOT;
-			return $"{fld} IS {sNot} NULL";
-		}//function
+		public static string IsNull(string fld, bool IsPositive = true) => $"{fld} IS {not(IsPositive)} NULL";
 
 		///<summary>fld In list</summary>
 		public static string In<T>(string fld, IEnumerable<T> list, bool IsPositive = true)
@@ -38,38 +36,22 @@ namespace BDB
 			else {
 				sList = string.Join(S.Comma, list.Select(s => s.ToString()));
 			}//else
-			string sNot = IsPositive ? string.Empty : NOT;
-			return $"{fld} {sNot} IN ({sList})";
+			return $"{fld} {not(IsPositive)} IN ({sList})";
 		}//function
 
 		///<summary>fld In select</summary>
-		public static string In(string fld, string select, bool IsPositive = true)
-		{
-			string sNot = IsPositive ? string.Empty : NOT;
-			return $"{fld} {sNot} IN ({select})";
-		}//function
-
+		public static string In(string fld, string select, bool IsPositive = true) => $"{fld} {not(IsPositive)} IN ({select})";
 		///<summary>fld LIKE pattern</summary>
-		public static string Like(string fld, string pattern, bool IsPositive = true)
-		{
-			string sNot = IsPositive ? string.Empty : NOT;
-			return $"{fld} {sNot} LIKE '{pattern}'";
-		}//function
-
+		public static string Like(string fld, string pattern, bool IsPositive = true) => $"{fld} {not(IsPositive)} LIKE '{pattern}'";
 		///<summary>fld LIKE par</summary>
-		public static string LikePar(string fld, string par, bool IsPositive = true)
-		{
-			string sNot = IsPositive ? string.Empty : NOT;
-			return $"{fld} {sNot} LIKE @{par}";
-		}//function
-		 ///<summary>fld LIKE par</summary>
-		public static string LikePar(string fld) { return LikePar(fld, fld, true); }
+		public static string LikePar(string fld, string par, bool IsPositive = true) => $"{fld} {not(IsPositive)} LIKE @{par}";
+		///<summary>fld LIKE par</summary>
+		public static string LikePar(string fld) => LikePar(fld, fld, true);
 
 		///<summary>fld between</summary>
 		public static string Between<T>(string fld, T valFrom, T valTo, bool IsPositive = true) where T: struct
 		{
-			string sNot = IsPositive ? string.Empty : NOT;
-			return $"{fld} {sNot} BETWEEN {valFrom} AND {valTo}";
+			return $"{fld} {not(IsPositive)} BETWEEN {valFrom} AND {valTo}";
 		}//function
 	}//class
 }
