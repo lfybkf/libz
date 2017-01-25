8a6c05d27a175eff344977a23df38266b54285e5
Булычев Даниил Борисович <bulychev.d@aetp.nn>
2017-01-25 10:30:29 +0300

    (bdb)

--- BDB.Extensions/build.bat
+++ BDB.Extensions/build.bat
@@ -1,1 +1,2 @@
-%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe BDB.Extensions.csproj > build.txt
+"C:\Program Files\MSBuild\14.0\Bin\msbuild.exe" BDB.Extensions.csproj > build.txt
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" BDB.Extensions.csproj > build.txt
